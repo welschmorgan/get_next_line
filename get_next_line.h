b0fb1454c3c78e55522cd70653f06128708dc656
Morgan WELSCH <mwelsch@e3r4p6.42.fr>
2016-03-27 13:44:32 +0200

    bugfix: implemented fd destruction after being read and processed (EOF received && lists are empty)

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/25 11:20:07 by mwelsch           #+#    #+#             */
-/*   Updated: 2016/03/27 13:12:39 by mwelsch          ###   ########.fr       */
+/*   Updated: 2016/03/27 13:40:50 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int				ft_push_fd(t_fd *fd);
 int				ft_process_fd(t_fd *fd, char **line);
 int				ft_read_fd(t_fd *fd);
 t_fd			*ft_reset_fd(t_fd *fd);
-
+void			ft_close_fd(t_dlist *fds, t_fd *fd);
 int				get_next_line(int const fd, char **line);
 
 #endif
